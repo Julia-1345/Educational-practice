f6df2b880855b24cb3218bdbc46b569c0b6020ac
Julia-1345 <miroshkina.yuli@gmail.com>
2025-06-30 13:24:58 +0300

    Реализация RSA
    Реализован алгоритм RSA и связанные с ним функции

--- main.py
+++ main.py
@@ -41,9 +41,53 @@ def modinv(e, phi):
         raise Exception('Обратного элемента не существует, gcd != 1')
     return x % phi
 
+def generate_rsa_keys():
+    p = int(input('Введите простое число p: '))
+    q = int(input('Введите простое число q: '))
+
+    if not is_prime(p):
+        raise ValueError(f"Число p={p} не является простым")
+    if not is_prime(q):
+        raise ValueError(f"Число q={q} не является простым")
+
+    n = p * q
+    print(f"Произведение n = p * q = {p} * {q} = {n}")
+    phi = (p - 1) * (q - 1)
+    print(f"Функция Эйлера phi(n) = (p-1)*(q-1) = {phi}")
+
+    for candidate in range(2, phi):
+        if gcd(candidate, phi) == 1:
+            e = candidate
+            print(f"Автоматически выбран открытый экспонент e = {e}")
+            break
+    else:
+        raise Exception('Не удалось найти подходящий e')
+
+    d = modinv(e, phi)
+    print(f"Закрытый ключ d = {d}")
+
+    return ((e, n), (d, n))
+
+def rsa_encrypt(message, pubkey):
+    e, n = pubkey
+    ciphertext = pow(message, e, n)
+    print(f"Шифротекст c = m^e mod n = {message}^{e} mod {n} = {ciphertext}")
+    return ciphertext
+
+def rsa_decrypt(ciphertext, privkey):
+    d, n = privkey
+    plaintext = pow(ciphertext, d, n)
+    print(f"Расшифрованное сообщение m = c^d mod n = {ciphertext}^{d} mod {n} = {plaintext}")
+    return plaintext
+
 def main():
     print('--- RSA Encryption ---')
+    pubkey, privkey = generate_rsa_keys()
 
+    m = int(input('Введите целочисленное сообщение m < n для шифрования: '))
+    c = rsa_encrypt(m, pubkey)
+    m2 = rsa_decrypt(c, privkey)
+    print(f"Оригинал: {m}, Расшифровано: {m2}")
     print('\n--- Diffie-Hellman Key Exchange ---')
 
 if __name__ == '__main__':
